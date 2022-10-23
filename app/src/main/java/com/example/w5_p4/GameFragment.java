4ae18f08391e24e9998465957ff341ad63419ef7
Eren Budur <erenbudur33@gmail.com>
2022-10-23 15:56:07 -0400

    added fragments

---
+++ app/src/main/java/com/example/w5_p4/GameFragment.java
@@ -0,0 +1,92 @@
+package com.example.w5_p4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GameFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private Button[][] buttons = new Button[4][4];
+
+    public GameFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GameFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GameFragment newInstance(String param1, String param2) {
+        GameFragment fragment = new GameFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        GridLayout gridLayout = view.findViewById(R.id.fragment_game_button_grid_layout);
+        //add a 4x4 grid of buttons to the gridlayout programmatically
+
+        ViewGroup.LayoutParams lp = gridLayout.getLayoutParams();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                buttons[i][j] = new Button(getActivity());
+                buttons[i][j].setText("0");
+                buttons[i][j].setTextSize(40);
+                buttons[i][j].setPadding(0, 0, 0, 0);
+                buttons[i][j].setBackgroundColor(getResources().getColor(R.color.white));
+                buttons[i][j].setLayout
+                gridLayout.addView(buttons[i][j]);
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/w5_p4/MainActivity.java
+++ app/src/main/java/com/example/w5_p4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.w5_p4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if( fragmentManager.findFragmentById( R.id.game_fragment ) == null ) {
+            androidx.fragment.app.FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            GameFragment fragment = new GameFragment();
+            transaction.add(R.id.game_fragment, fragment );
+            transaction.commit( );
+        }
+
+        if(fragmentManager.findFragmentById(R.id.score_fragment) == null) {
+            androidx.fragment.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+            ScoreFragment fragment = new ScoreFragment();
+            transaction.add(R.id.score_fragment, fragment);
+            transaction.commit();
+        }
+
     }
+
+
+
+
+
 }
